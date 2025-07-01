2db823271b027492e9f7530b117b36f8c25ede84
Relix18 <relix049@gmail.com>
2025-07-01 21:37:57 +0530

    ai jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,28 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { toast } from "sonner";
 
 export default function Home() {
+  const [value, setValue] = useState("");
+
   const trpc = useTRPC();
-  const invoke = useMutation(trpc.invoke.mutationOptions({}));
+  const invoke = useMutation(
+    trpc.invoke.mutationOptions({
+      onSuccess: () => {
+        toast.success("Background job started");
+      },
+    })
+  );
 
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <Button onClick={() => invoke.mutate({ text: "Relix" })}>
-        Invoke Inngest
-      </Button>
+    <div className="flex p-6 gap-2 items-center">
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
+      <Button onClick={() => invoke.mutate({ value })}>Invoke Inngest</Button>
     </div>
   );
 }
