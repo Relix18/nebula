e9dc33d518f06353d9cf59d4ad2e720772a631cc
Relix18 <relix049@gmail.com>
2025-07-02 14:16:50 +0530

    3: e2b sandbox

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,16 @@
 import { gemini, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
+import { Sandbox } from "@e2b/code-interpreter";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("nebula-nextjs-test2");
+      return sandbox.sandboxId;
+    });
     const codeAgent = createAgent({
       name: "code-agent",
       system:
@@ -15,7 +21,11 @@ export const helloWorld = inngest.createFunction(
       "Write the following snippet" + event.data.value
     );
 
-    console.log(output);
-    return { output };
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+    return { output, sandboxUrl };
   }
 );
