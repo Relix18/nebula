d60cc00bdd973592b4e0489ead7d33a56582e6f6
Relix18 <relix049@gmail.com>
2025-07-05 14:36:25 +0530

    8: messages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import { toast } from "sonner";
 
@@ -11,8 +11,9 @@ export default function Home() {
   const [value, setValue] = useState("");
 
   const trpc = useTRPC();
+  const messages = useQuery(trpc.messages.getMany.queryOptions());
   const invoke = useMutation(
-    trpc.invoke.mutationOptions({
+    trpc.messages.create.mutationOptions({
       onSuccess: () => {
         toast.success("Background job started");
       },
@@ -20,9 +21,12 @@ export default function Home() {
   );
 
   return (
-    <div className="flex p-6 gap-2 items-center">
-      <Input value={value} onChange={(e) => setValue(e.target.value)} />
-      <Button onClick={() => invoke.mutate({ value })}>Invoke Inngest</Button>
-    </div>
+    <>
+      <div className="flex p-6 gap-2 items-center">
+        <Input value={value} onChange={(e) => setValue(e.target.value)} />
+        <Button onClick={() => invoke.mutate({ value })}>Invoke Inngest</Button>
+      </div>
+      <p>{JSON.stringify(messages.data)}</p>
+    </>
   );
 }
