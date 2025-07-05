c27681af92c7d28d96acdd3379577738b0cd8db3
Relix18 <relix049@gmail.com>
2025-07-05 16:35:52 +0530

    9: projects

---
+++ prisma/migrations/20250705102657_projects/migration.sql
@@ -0,0 +1,27 @@
+/*
+  Warnings:
+
+  - Added the required column `projectId` to the `Message` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Fragment" DROP CONSTRAINT "Fragment_messageId_fkey";
+
+-- AlterTable
+ALTER TABLE "Message" ADD COLUMN     "projectId" TEXT NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Fragment" ADD CONSTRAINT "Fragment_messageId_fkey" FOREIGN KEY ("messageId") REFERENCES "Message"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -163,6 +163,7 @@ export const codeAgentFunction = inngest.createFunction(
       if (isError) {
         return await prisma.message.create({
           data: {
+            projectId: event.data.projectId,
             content: "An error occurred while processing your request.",
             role: "ASSISTANT",
             type: "ERROR",
@@ -171,6 +172,7 @@ export const codeAgentFunction = inngest.createFunction(
       }
       return await prisma.message.create({
         data: {
+          projectId: event.data.projectId,
           content: result.state.data.summary,
           role: "ASSISTANT",
           type: "RESULT",
